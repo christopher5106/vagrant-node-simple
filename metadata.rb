e0c993dcb19f86f43d4ce2f0a4984862c4717131
Bourez <christopher.bourez@axa-direct.com>
2015-10-07 15:03:32 +0200

    n

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name "vagrant-node-simple"
-version "0.1.17"
+version "0.1.18"
 description "A simple cookbook to deploy your NodeJS app on Vagrant Ubuntu"
 
 depends 'apt'
--- recipes/gype.rb
+++ recipes/gype.rb
@@ -1,8 +1,8 @@
-bash "npm packages install" do
+bash "npm-gype-install" do
   user "root"
-  cwd "/srv/www/#{node[:nodejs][:name]}/current"
+  cwd "/home/ubuntu"
   code <<-EOH
   apt-get -y install node-gyp
-  echo "unsafe-perm = true" >> ~/.npmrc
+  echo "unsafe-perm = true" >> /home/ubuntu/.npmrc
   EOH
 end
