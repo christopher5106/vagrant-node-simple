bd1e4398835183567db43b1b031c5760b10baf16
Bourez <christopher.bourez@axa-direct.com>
2015-10-09 10:43:43 +0200

    n

--- recipes/gype.rb
+++ recipes/gype.rb
@@ -2,7 +2,7 @@ bash "npm-gype-install" do
   user "root"
   cwd "/home/ubuntu"
   code <<-EOH
-  apt-get -y install node-gyp
+  npm install -g node-gyp
   echo "unsafe-perm = true" >> /home/ubuntu/.npmrc
   EOH
 end
--- recipes/install.rb
+++ recipes/install.rb
@@ -3,11 +3,5 @@ bash "npm packages install" do
   cwd "/home/ubuntu/"
   code <<-EOH
     curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
-    apt-get -y update
   EOH
 end
-
-# package 'nodejs'
-# package 'npm'
-# package 'build-essential'
-# package 'nodejs-legacy'
